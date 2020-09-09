b14e9f3e286918715e4af4785346f1e6f63d85b3
Vincent Flyer (Vincent Chan) <35939457+VFlyer@users.noreply.github.com>
2020-09-09 14:41:04 -0700

    Mastermind Restricted Minor Changes
    * Check on unused assets and avoid bundling those

--- Assets/MastermindRestricted/MastermindRestrictedCore.cs
+++ Assets/MastermindRestricted/MastermindRestrictedCore.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using UnityEngine;
-using UnityEngine.UI;
 using uernd = UnityEngine.Random;
 
 public class MastermindRestrictedCore : MonoBehaviour {
