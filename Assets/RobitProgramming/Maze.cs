7dabbd5253dd6e2963ce9c9f3c073ef71786e63f
Vincent Flyer (Vincent Chan) <35939457+VFlyer@users.noreply.github.com>
2020-11-04 17:21:35 -0800

    Minor Changes November 4th 2020
    * Ensure only to use specific directives in one of the scripts
    * PDF update.

--- Assets/RobitProgramming/Maze.cs
+++ Assets/RobitProgramming/Maze.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditorInternal;
-using UnityEngine;
+﻿using System.Collections;
 
 public partial class Maze {
 
