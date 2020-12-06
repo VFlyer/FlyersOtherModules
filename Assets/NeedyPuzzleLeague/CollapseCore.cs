7395266d30522283720ee3fb9dc7611fb673913d
Vincent Flyer (Vincent Chan) <35939457+VFlyer@users.noreply.github.com>
2020-12-06 15:23:58 -0800

    Update Collapse Basic
    * Unlit Texture + Color addition
    * Also add textures for the module

--- Assets/NeedyPuzzleLeague/CollapseCore.cs
+++ Assets/NeedyPuzzleLeague/CollapseCore.cs
@@ -18,7 +18,7 @@ public class CollapseCore : MonoBehaviour {
     public RowRendererPlus[] rowRenderersWithSelectables;
     public RowRenderers rowRenderersNextSet;
 
-    Color[] colorList = {
+    public Color[] colorList = {
         Color.cyan,
         Color.blue,
         Color.gray,
@@ -26,13 +26,15 @@ public class CollapseCore : MonoBehaviour {
         Color.red,
         Color.magenta
     };
-    string colorString = "CBAYRM";
+    public string colorString = "CBAYRM";
+    public Texture[] colorblindTextures;
+    int[] colorIdxSet = { 0, 1, 2, 3, 4, 5 };
 
-    bool[,] isSelected = new bool[9, 13];
-    int[,] colorIdxAll = new int[9, 13];
+    bool[,] isSelected = new bool[10, 15];
+    int[,] colorIdxBoard = new int[10, 15];
     static int modIdCounter = 1;
     int modID;
-    bool isActive = false, permDeactivate;
+    bool isActive = false, permDeactivate, pauseLift = false, interactable = false;
     List<Vector3> startRowCoords = new List<Vector3>();
     float timeLeftManaged = 10f, dynamicRate = .8f;
 
@@ -50,6 +52,7 @@ public class CollapseCore : MonoBehaviour {
                 timeManager = ModifyNeedyTimer();
                 StartCoroutine(blockLifter);
                 StartCoroutine(timeManager);
+                interactable = true;
             }
             else
                 needySelf.HandlePass();
@@ -68,7 +71,7 @@ public class CollapseCore : MonoBehaviour {
                 rowRenderer.rowSelectables[x].OnInteract += delegate {
                     rowRenderer.rowSelectables[tRow].AddInteractionPunch(0.1f);
                     mAudio.PlaySoundAtTransform("tick", rowRenderer.rowSelectables[tRow].transform);
-                    if (isActive)
+                    if (isActive && interactable)
                         ProcessTile(tRow, tCol);
                     return false;
                 };
@@ -97,6 +100,7 @@ public class CollapseCore : MonoBehaviour {
         rowRenderersNextSet.transform.localPosition = new Vector3(0, 0, -6.5f);
         rowRenderersNextSet.transform.localScale = new Vector3(1, 1, 0);
         rowRenderersNextSet.gameObject.SetActive(false);
+        colorIdxSet.Shuffle();
     }
     void QuickLog(string value)
     {
@@ -105,18 +109,18 @@ public class CollapseCore : MonoBehaviour {
 
     void LogBoard()
     {
-        QuickLog("o---------o");
-        for (int y = colorIdxAll.GetLength(1) - 1; y >= 0; y--)
+        QuickLog("o----------o");
+        for (int y = colorIdxBoard.GetLength(1) - 1; y >= 0; y--)
         {
             List<string> valuesSet = new List<string>();
-            for (int x = 0; x < colorIdxAll.GetLength(0); x++)
+            for (int x = 0; x < colorIdxBoard.GetLength(0); x++)
             {
-                int value = colorIdxAll[x, y];
+                int value = colorIdxBoard[x, y];
                 valuesSet.Add(value == 0 ? " " : colorString[value - 1].ToString());
             }
             QuickLog(string.Format("|{0}|", valuesSet.Join("")));
         }
-        QuickLog("o---------o");
+        QuickLog("o----------o");
     }
 
     void HandleDeactivation()
@@ -137,9 +141,9 @@ public class CollapseCore : MonoBehaviour {
     {
         if (!isSelected[row, col])
         {
-            if (colorIdxAll[row, col] == 0) return;
+            if (colorIdxBoard[row, col] == 0) return;
             DeselectAllTiles();
-            SelectAllSimilarTiles(row, col, colorIdxAll[row, col]);
+            SelectAllSimilarTiles(row, col, colorIdxBoard[row, col]);
             if (CountSelectedTiles() < 3)
             {
                 DeselectAllTiles();
@@ -147,31 +151,74 @@ public class CollapseCore : MonoBehaviour {
         }
         else
         {
+            /*
             for (int x = 0; x < isSelected.GetLength(0); x++)
                 for (int y = 0; y < isSelected.GetLength(1); y++)
                 {
                     if (isSelected[x, y])
-                        colorIdxAll[x, y] = 0;
+                        colorIdxBoard[x, y] = 0;
                 }
-            StopCoroutine(blockLifter);
-            StopCoroutine(timeManager);
             DeselectAllTiles();
-            CollaspeAllTiles();
-            StartCoroutine(blockLifter);
+            CollapseAllTiles();
+            */
+            //StopCoroutine(blockLifter);
+            StopCoroutine(timeManager);
+            pauseLift = true;
+            interactable = false;
+            
+            StartCoroutine(AnimateBreakingAnim());
             StartCoroutine(timeManager);
         }
         UpdateGrid();
     }
-    void CollaspeAllTiles()
+    IEnumerator AnimateBreakingAnim()
     {
-        for (int x = 0; x < colorIdxAll.GetLength(0); x++)
-            for (int y = 0; y < colorIdxAll.GetLength(1) - 1; y++)
-                for (int z = 0; z < colorIdxAll.GetLength(1) - 1; z++)
+        List<int[]> coordSet = new List<int[]>();
+
+        for (int x = 0; x < isSelected.GetLength(0); x++)
+            for (int y = 0; y < isSelected.GetLength(1); y++)
+            {
+                if (isSelected[x, y])
                 {
-                    if (colorIdxAll[x, z] == 0)
+                    coordSet.Add(new[] { x, y });
+                    
+                }
+            }
+        foreach (int[] oneCoord in coordSet.Shuffle())
+        {
+            colorIdxBoard[oneCoord[0], oneCoord[1]] = 0;
+            UpdateGrid();
+            yield return new WaitForSeconds(0.01f);
+        }
+
+        DeselectAllTiles();
+        for (int y = 0; y < colorIdxBoard.GetLength(1) - 1; y++)
+            for (int x = 0; x < colorIdxBoard.GetLength(0); x++)
+                for (int z = 0; z < colorIdxBoard.GetLength(1) - 1; z++)
+                {
+                    if (colorIdxBoard[x, z] == 0 && colorIdxBoard[x, z + 1] != 0)
                     {
-                        colorIdxAll[x, z] = colorIdxAll[x, z + 1];
-                        colorIdxAll[x, z + 1] = 0;
+                        colorIdxBoard[x, z] = colorIdxBoard[x, z + 1];
+                        colorIdxBoard[x, z + 1] = 0;
+                        UpdateGrid();
+                        yield return new WaitForSeconds(0.01f);
+                    }
+                }
+        yield return null;
+        pauseLift = false;
+        interactable = true;
+
+    }
+    void CollapseAllTiles()
+    {
+        for (int x = 0; x < colorIdxBoard.GetLength(0); x++)
+            for (int y = 0; y < colorIdxBoard.GetLength(1) - 1; y++)
+                for (int z = 0; z < colorIdxBoard.GetLength(1) - 1; z++)
+                {
+                    if (colorIdxBoard[x, z] == 0)
+                    {
+                        colorIdxBoard[x, z] = colorIdxBoard[x, z + 1];
+                        colorIdxBoard[x, z + 1] = 0;
                     }
                 }
     }
@@ -181,24 +228,24 @@ public class CollapseCore : MonoBehaviour {
         for (float t = 0; t <= 1f; t = Mathf.Min(t + Time.deltaTime, 1f))
         {
             yield return null;
-            for (int y = colorIdxAll.GetLength(1) - 1; y >= 0; y--)
+            for (int y = colorIdxBoard.GetLength(1) - 1; y >= 0; y--)
             {
-                for (int x = 0; x < colorIdxAll.GetLength(0); x++)
+                for (int x = 0; x < colorIdxBoard.GetLength(0); x++)
                 {
-                    int curColorIdx = colorIdxAll[x, y];
+                    int curColorIdx = colorIdxBoard[x, y];
                     if (curColorIdx != 0)
                     {
-                        rowRenderersWithSelectables[y].wallRenderers[x].material.color = colorList[curColorIdx - 1] * 0.5f * (1 - t) + Color.white * 0.5f * (1 - t);
+                        rowRenderersWithSelectables[y].wallRenderers[x].material.color = colorList[colorIdxSet[curColorIdx - 1]] * 0.5f * (1 - t) + Color.white * 0.5f * (1 - t);
                     }
                 }
             }
             if (t >= 1f) break;
         }
-        for (int y = colorIdxAll.GetLength(1) - 1; y >= 0; y--)
+        for (int y = colorIdxBoard.GetLength(1) - 1; y >= 0; y--)
         {
-            for (int x = 0; x < colorIdxAll.GetLength(0); x++)
+            for (int x = 0; x < colorIdxBoard.GetLength(0); x++)
             {
-                colorIdxAll[x, y] = 0;
+                colorIdxBoard[x, y] = 0;
             }
         }
         UpdateGrid();
@@ -218,11 +265,11 @@ public class CollapseCore : MonoBehaviour {
         {
             yield return null;
             needySelf.SetNeedyTimeRemaining(timeLeftManaged * 9.9f);
-            int[] heightCounts = new int[9];
-            for (int x = 0; x < colorIdxAll.GetLength(0); x++)
-                for (int y = 0; y < colorIdxAll.GetLength(1); y++)
-                    heightCounts[x] += colorIdxAll[x, y] != 0 ? 1 : 0;
-            if (heightCounts.Max() < 13)
+            int[] heightCounts = new int[10];
+            for (int x = 0; x < colorIdxBoard.GetLength(0); x++)
+                for (int y = 0; y < colorIdxBoard.GetLength(1); y++)
+                    heightCounts[x] += colorIdxBoard[x, y] != 0 ? 1 : 0;
+            if (heightCounts.Max() < 15)
                 timeLeftManaged = Mathf.Min(10f, timeLeftManaged + Time.deltaTime);
         }
     }
@@ -233,15 +280,16 @@ public class CollapseCore : MonoBehaviour {
         while (isActive)
         {
             yield return null;
-            int[] heightCounts = new int[9];
-            for (int x = 0; x < colorIdxAll.GetLength(0); x++)
-                for (int y = 0; y < colorIdxAll.GetLength(1); y++)
-                    heightCounts[x] += colorIdxAll[x, y] != 0 ? 1 : 0;
-            if (heightCounts.Max() < 13)
+            int[] heightCounts = new int[10];
+            for (int x = 0; x < colorIdxBoard.GetLength(0); x++)
+                for (int y = 0; y < colorIdxBoard.GetLength(1); y++)
+                    heightCounts[x] += colorIdxBoard[x, y] != 0 ? 1 : 0;
+            if (heightCounts.Max() < 15)
                 yield return AnimateNextRowDynamicAnim();
             else
             {
-                timeLeftManaged -= Time.deltaTime;
+                if (!pauseLift)
+                    timeLeftManaged -= Time.deltaTime;
                 //mAudio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.NeedyWarning, transform);
                 if (timeLeftManaged < 0f)
                 {
@@ -256,43 +304,44 @@ public class CollapseCore : MonoBehaviour {
 
         yield return null;
 
-        int maxColorsPossible = 2 + (maxSolvables * 3 < currentlySolved * 10 ? 1 : 0) + (maxSolvables * 9 < currentlySolved * 10 ? 1 : 0);
+        int maxColorsPossible = 2 + (maxSolvables * 1 < currentlySolved * 2 ? 1 : 0) + (maxSolvables * 9 < currentlySolved * 10 ? 1 : 0);
 
-        int[] nextRow = new int[9];
+        int[] nextRow = new int[10];
         for (int x = 0; x < nextRow.Length; x++)
         {
             nextRow[x] = uernd.Range(1, maxColorsPossible + 1);
         }
         rowRenderersNextSet.gameObject.SetActive(true);
-        for (float x = 0; x < 1f; x += Time.deltaTime * dynamicRate * Mathf.Min(1f, (currentlySolved + 1f) / maxSolvables))
+        for (float x = 0; x < 1f; x += Time.deltaTime * dynamicRate * Mathf.Min(1f, (currentlySolved + 1f) / maxSolvables) * (pauseLift ? 0f : 1f))
         {
             yield return null;
-            rowRenderersNextSet.transform.localPosition = new Vector3(0, 0, -6f - (0.5f * (1f - x)));
+            rowRenderersNextSet.transform.localPosition = new Vector3(0, 0.1f, -7f - (0.5f * (1f - x)));
             rowRenderersNextSet.transform.localScale = new Vector3(1, 1, x);
 
             currentlySolved = bombInfo.GetSolvedModuleIDs().Count();
 
             for (int z = 0; z < rowRenderersNextSet.wallRenderers.Length; z++)
             {
-                rowRenderersNextSet.wallRenderers[z].material.color = colorList[nextRow[z] - 1] * 0.5f +
+                rowRenderersNextSet.wallRenderers[z].material.color = colorList[colorIdxSet[nextRow[z] - 1]] * 0.5f +
                     Color.gray * 0.5f;
+                rowRenderersNextSet.wallRenderers[z].material.SetTexture("_MainTex", colorblindTextures[colorIdxSet[nextRow[z] - 1]]);
             }
             for (int y = 0; y < rowRenderersWithSelectables.Length; y++)
             {
                 rowRenderersWithSelectables[y].transform.localPosition = startRowCoords[y] + new Vector3(0, 0, x);
             }
         }
-        for (int x = 0; x < colorIdxAll.GetLength(0); x++)
-            for (int y = colorIdxAll.GetLength(1) - 1; y >= 0; y--)
+        for (int x = 0; x < colorIdxBoard.GetLength(0); x++)
+            for (int y = colorIdxBoard.GetLength(1) - 1; y >= 0; y--)
             {
                 if (y > 0)
                 {
-                    colorIdxAll[x, y] = colorIdxAll[x, y - 1];
+                    colorIdxBoard[x, y] = colorIdxBoard[x, y - 1];
                     isSelected[x, y] = isSelected[x, y - 1];
                 }
                 else
                 {
-                    colorIdxAll[x, y] = nextRow[x];
+                    colorIdxBoard[x, y] = nextRow[x];
                     isSelected[x, y] = false;
                 }
             }
@@ -300,7 +349,7 @@ public class CollapseCore : MonoBehaviour {
         {
             rowRenderersWithSelectables[y].transform.localPosition = startRowCoords[y];
         }
-        rowRenderersNextSet.transform.localPosition = new Vector3(0, 0, -6.5f);
+        rowRenderersNextSet.transform.localPosition = new Vector3(0, 0, -7.5f);
         rowRenderersNextSet.transform.localScale = new Vector3(1, 1, 0);
         rowRenderersNextSet.gameObject.SetActive(false);
         UpdateGrid();
@@ -309,7 +358,7 @@ public class CollapseCore : MonoBehaviour {
     {
         if (speed <= 0f) yield break;
         yield return null;
-        int[] nextRow = new int[9];
+        int[] nextRow = new int[10];
         for (int x = 0; x < nextRow.Length; x++)
         {
             nextRow[x] = uernd.Range(1, 3);
@@ -318,30 +367,31 @@ public class CollapseCore : MonoBehaviour {
         for (float x = 0; x < 1f; x += Time.deltaTime * speed)
         {
             yield return null;
-            rowRenderersNextSet.transform.localPosition = new Vector3(0, 0, -6f - (0.5f * (1f - x)));
+            rowRenderersNextSet.transform.localPosition = new Vector3(0, 0.1f, -7f - (0.5f * (1f - x)));
             rowRenderersNextSet.transform.localScale = new Vector3(1, 1, x);
 
             for (int z = 0; z < rowRenderersNextSet.wallRenderers.Length; z++)
             {
-                rowRenderersNextSet.wallRenderers[z].material.color = colorList[nextRow[z] - 1] * 0.5f +
+                rowRenderersNextSet.wallRenderers[z].material.color = colorList[colorIdxSet[nextRow[z] - 1]] * 0.5f +
                     Color.gray *  0.5f;
+                rowRenderersNextSet.wallRenderers[z].material.SetTexture("_MainTex", colorblindTextures[colorIdxSet[nextRow[z] - 1]]);
             }
             for (int y = 0; y < rowRenderersWithSelectables.Length; y++)
             {
                 rowRenderersWithSelectables[y].transform.localPosition = startRowCoords[y] + new Vector3(0, 0, x);
             }
         }
-        for (int x = 0; x < colorIdxAll.GetLength(0); x++)
-            for (int y = colorIdxAll.GetLength(1) - 1; y >= 0; y--)
+        for (int x = 0; x < colorIdxBoard.GetLength(0); x++)
+            for (int y = colorIdxBoard.GetLength(1) - 1; y >= 0; y--)
             {
                 if (y > 0)
                 {
-                    colorIdxAll[x, y] = colorIdxAll[x, y - 1];
+                    colorIdxBoard[x, y] = colorIdxBoard[x, y - 1];
                     isSelected[x, y] = isSelected[x, y - 1];
                 }
                 else
                 {
-                    colorIdxAll[x, y] = nextRow[x];
+                    colorIdxBoard[x, y] = nextRow[x];
                     isSelected[x, y] = false;
                 }
             }
@@ -349,7 +399,7 @@ public class CollapseCore : MonoBehaviour {
         {
             rowRenderersWithSelectables[y].transform.localPosition = startRowCoords[y];
         }
-        rowRenderersNextSet.transform.localPosition = new Vector3(0, 0, -6.5f);
+        rowRenderersNextSet.transform.localPosition = new Vector3(0, 0, -7.5f);
         rowRenderersNextSet.transform.localScale = new Vector3(1, 1, 0);
         rowRenderersNextSet.gameObject.SetActive(false);
         UpdateGrid();
@@ -376,22 +426,22 @@ public class CollapseCore : MonoBehaviour {
         isSelected[curX, curY] = true;
         if (curX - 1 >= 0)
         {
-            if (colorIdxAll[curX - 1, curY] == idxColor && !isSelected[curX - 1, curY])
+            if (colorIdxBoard[curX - 1, curY] == idxColor && !isSelected[curX - 1, curY])
                 SelectAllSimilarTiles(curX - 1, curY, idxColor);
         }
-        if (curX + 1 < colorIdxAll.GetLength(0))
+        if (curX + 1 < colorIdxBoard.GetLength(0))
         {
-            if (colorIdxAll[curX + 1, curY] == idxColor && !isSelected[curX + 1, curY])
+            if (colorIdxBoard[curX + 1, curY] == idxColor && !isSelected[curX + 1, curY])
                 SelectAllSimilarTiles(curX + 1, curY, idxColor);
         }
         if (curY - 1 >= 0)
         {
-            if (colorIdxAll[curX, curY - 1] == idxColor && !isSelected[curX, curY - 1])
+            if (colorIdxBoard[curX, curY - 1] == idxColor && !isSelected[curX, curY - 1])
                 SelectAllSimilarTiles(curX, curY - 1, idxColor);
         }
-        if (curY + 1 < colorIdxAll.GetLength(1))
+        if (curY + 1 < colorIdxBoard.GetLength(1))
         {
-            if (colorIdxAll[curX, curY + 1] == idxColor && !isSelected[curX, curY + 1])
+            if (colorIdxBoard[curX, curY + 1] == idxColor && !isSelected[curX, curY + 1])
                 SelectAllSimilarTiles(curX, curY + 1, idxColor);
         }
         return;
@@ -405,7 +455,28 @@ public class CollapseCore : MonoBehaviour {
         }
         yield break;
     }
-	void RequestDetonation()
+
+    void UpdateGrid()
+    {
+        for (int x = 0; x < rowRenderersWithSelectables.Length; x++)
+        {
+            for (int y = 0; y < rowRenderersWithSelectables[x].wallRenderers.Length; y++)
+            {
+                int idxShown = colorIdxBoard[y, x];
+                rowRenderersWithSelectables[x].wallRenderers[y].enabled = idxShown > 0;
+                rowRenderersWithSelectables[x].wallRenderers[y].material.color =
+                    (idxShown > 0 ? colorList[colorIdxSet[idxShown - 1]] : Color.black) * (isSelected[y, x] ? 1 : 0.5f) +
+                    Color.white * (isSelected[y, x] ? 0f : 0.5f);
+                if (idxShown > 0)
+                    rowRenderersWithSelectables[x].wallRenderers[y].material.SetTexture("_MainTex", colorblindTextures[colorIdxSet[idxShown - 1]]);
+            }
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+	}
+    void RequestDetonation()
     {
         QuickLog("You let the needy run out. The bomb goes along as well.");
         if (Application.isEditor)
@@ -447,23 +518,4 @@ public class CollapseCore : MonoBehaviour {
                 Debug.LogFormat("can't find component");
         }
     }
-
-    void UpdateGrid()
-    {
-        for (int x = 0; x < rowRenderersWithSelectables.Length; x++)
-        {
-            for (int y = 0; y < rowRenderersWithSelectables[x].wallRenderers.Length; y++)
-            {
-                int idxShown = colorIdxAll[y, x];
-                rowRenderersWithSelectables[x].wallRenderers[y].enabled = idxShown > 0;
-                rowRenderersWithSelectables[x].wallRenderers[y].material.color =
-                    (idxShown > 0 ? colorList[idxShown - 1] : Color.black) * (isSelected[y, x] ? 1 : 0.5f) +
-                    Color.white * (isSelected[y, x] ? 0f : 0.5f);
-            }
-        }
-    }
-
-	// Update is called once per frame
-	void Update () {
-	}
 }
