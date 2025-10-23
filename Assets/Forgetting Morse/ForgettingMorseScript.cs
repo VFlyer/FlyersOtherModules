ada26b4515d6fb01fc22eb1b0205014ce21ec9b1
Vincent Flyer (Vincent Chan) <35939457+VFlyer@users.noreply.github.com>
2025-10-22 23:02:00 -0700

    Various Updates I Forgot.
    - Move Robit Programming to https://github.com/VFlyer/FlyersHarderBossModules
    - Theorycraft Forgetting Morse
    - Update Steam Workshop Icon for update later

---
+++ Assets/Forgetting Morse/ForgettingMorseScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForgettingMorseScript : MonoBehaviour {
+
+	public KMBossModule bossHandler;
+	public KMBombModule modSelf;
+	public KMBombInfo bombInfo;
+	public KMAudio mAudio;
+	public KMSelectable[] keyboardBtns;
+	public TextMesh shortTxt, longTxt;
+
+	static int modIDCnt;
+	int moduleID;
+	bool focused, moduleSolved, activated;
+
+	private static Dictionary<char, string> chrMorse = new Dictionary<char, string> {
+		{ 'A', ".-" }, { 'B', "-..." }, { 'C', "-.-." }, { 'D', "-.." },
+		{ 'E', "." }, { 'F', "..-." }, { 'G', "--." }, { 'H', "...." },
+		{ 'I', ".." }, { 'J', ".---" }, { 'K', "-.-" }, { 'L', ".-.." },
+		{ 'M', "--" }, { 'N', "-." }, { 'O', "---" }, { 'P', ".--." },
+		{ 'Q', "--.-" }, { 'R', ".-." }, { 'S', "..." }, { 'T', "-" },
+		{ 'U', "..-" }, { 'V', "...-" }, { 'W', ".--" }, { 'X', "-..-" },
+		{ 'Y', "-.--" }, { 'Z', "--.." },
+		{ '0', "-----" }, { '1', ".----" }, { '2', "..---" },
+		{ '3', "...--" }, { '4', "....-" }, { '5', "....." },
+		{ '6', "-...." }, { '7', "--..." }, { '8', "---.." },
+		{ '9', "----." },
+	};
+	void QuickLog(string value, params object[] args)
+	{
+		Debug.LogFormat("[Forgetting Morse #{0}] {1}", moduleID, string.Format(value, args));
+	}
+	void QuickLogDebug(string value, params object[] args)
+	{
+		Debug.LogFormat("<Forgetting Morse #{0}> {1}", moduleID, string.Format(value, args));
+	}
+	// Use this for initialization
+	void Start () {
+		moduleID = ++modIDCnt;
+		modSelf.OnActivate += ActivateModule;
+	}
+	void ActivateModule()
+    {
+		activated = true;
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/KMBossModule.cs
+++ Assets/Scripts/KMBossModule.cs
@@ -4,6 +4,34 @@ using UnityEngine;
 
 public class KMBossModule : MonoBehaviour
 {
+    public string[] GetIgnoredModuleIDs(KMBombModule module, string[] @default = null)
+    {
+        return GetIgnoredModuleIDs(module.ModuleDisplayName, @default);
+    }
+
+    public string[] GetIgnoredModuleIDs(string moduleDisplayName, string[] @default = null)
+    {
+        if (Application.isEditor)
+            return @default ?? new string[0];
+
+        var bossModuleManagerAPIGameObject = GameObject.Find("BossModuleManager");
+        if (bossModuleManagerAPIGameObject == null) // Boss Module Manager is not installed
+        {
+            Debug.LogFormat(@"[KMBossModule] Boss Module Manager is not installed.");
+            return @default ?? new string[0];
+        }
+
+        var bossModuleManagerAPI = bossModuleManagerAPIGameObject.GetComponent<IDictionary<string, object>>();
+        if (bossModuleManagerAPI == null || !bossModuleManagerAPI.ContainsKey("GetIgnoredModuleIDs"))
+        {
+            Debug.LogFormat(@"[KMBossModule] Boss Module Manager does not have a list on record for “{0}”.", moduleDisplayName);
+            return @default ?? new string[0];
+        }
+
+        var list = ((Func<string, string[]>) bossModuleManagerAPI["GetIgnoredModuleIDs"])(moduleDisplayName);
+        Debug.LogFormat(@"[KMBossModule] Boss Module Manager returned list for “{0}”: {1}", moduleDisplayName, list == null ? "<null>" : list.Join(", "));
+        return list ?? @default ?? new string[0];
+    }
     public string[] GetIgnoredModules(KMBombModule module, string[] @default = null)
     {
         return GetIgnoredModules(module.ModuleDisplayName, @default);
